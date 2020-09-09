91d9ebbcfe15250729f990d5a097b294f947b38a
kev127 <kelvinkeya125@gmail.com>
2020-09-09 16:16:47 +0300

    setting configurations to init.py

--- app/views.py
+++ app/views.py
@@ -8,5 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'Home - Welcome To Prime News'
     message = 'Welcome To Prime News'
-    return render_template('index.html' ,message = message)
+    return render_template('index.html' ,message = message, title = title)
