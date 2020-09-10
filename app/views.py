87ea0715cdefb5d3a02d7f9475e7103aa37b048b
kev127 <kelvinkeya125@gmail.com>
2020-09-10 13:56:44 +0300

    add get news function to display to index file template

--- app/views.py
+++ app/views.py
@@ -8,6 +8,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    news = get_news('business')
+	sports_news = get_news('sports')
+	technology_news = get_news('technology')
+	entertainment_news = get_news('entertainment')
     title = 'Home - Welcome To Prime News'
     message = 'Welcome To Prime News'
-    return render_template('index.html' ,message = message, title = title)
+    return render_template('index.html' ,message = message, title = title, news = news, sports_news = sports_news, technology_news = technology_news, entertainment_news = entertainment_news) )
