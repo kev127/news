3a3fd7d0b039d2525005c78eb09b99cef7bb4fc2
kev127 <kelvinkeya125@gmail.com>
2020-09-10 08:21:31 +0300

    add new folder models and news_test.py

---
+++ app/news_test.py
@@ -0,0 +1,21 @@
+import unittest
+from models import movie
+News = news.News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = news(,'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+
+if __name__ == '__main__':
+    unittest.main()
