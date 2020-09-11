056f12747d7a7f9dd5ed3178a39d9f2cb259df10
kev127 <kelvinkeya125@gmail.com>
2020-09-11 22:42:36 +0300

    import os to app

--- app/config.py
+++ app/config.py
@@ -1,9 +1,11 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    pass
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 
@@ -26,3 +28,9 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
