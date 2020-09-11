34e74bd5f63fa2c2119bc564c2d07203f85aef19
kev127 <kelvinkeya125@gmail.com>
2020-09-11 23:10:28 +0300

    moved config.py from app directory to root directory

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,14 @@ from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+#create the app configurations
+app.config.from_object(config_options[config_name])
+
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
 
