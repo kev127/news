0133b94220c6cd317aeb615ff2292b649f393bed
kev127 <kelvinkeya125@gmail.com>
2020-09-12 10:44:29 +0300

    import the configure_request function from request.py to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -24,4 +24,9 @@ bootstrap.init_app(app)
 from .main import main as main_blueprint
 app.register_blueprint(main_blueprint)
 
+# setting config
+from .requests import configure_request
+configure_request(app)
+
+
 return app
